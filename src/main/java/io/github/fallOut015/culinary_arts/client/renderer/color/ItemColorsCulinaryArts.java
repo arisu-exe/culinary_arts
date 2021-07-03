eab213e7eb3a5ff4e63ab55cada729ec21d3d6bd
fallOut015 <captainDoodles264@gmail.com>
2021-07-03 13:37:33 -0700

    Update ItemColorsCulinaryArts.java

--- src/main/java/io/github/fallOut015/culinary_arts/client/renderer/color/ItemColorsCulinaryArts.java
+++ src/main/java/io/github/fallOut015/culinary_arts/client/renderer/color/ItemColorsCulinaryArts.java
@@ -7,19 +7,18 @@ import io.github.fallOut015.culinary_arts.item.PizzaItem;
 import net.minecraft.client.Minecraft;
 import net.minecraft.item.DyeColor;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.stream.Collectors;
 
 public class ItemColorsCulinaryArts {
-    public static void doClientStuff(final FMLClientSetupEvent event) {
-        Minecraft minecraft = event.getMinecraftSupplier().get();
-
+    public static void onColorHandlerItem(ColorHandlerEvent.Item event) {
         for(Item item : ForgeRegistries.ITEMS.getValues().stream().filter(item -> item instanceof MultiColoredItem).collect(Collectors.toList())) {
-            minecraft.getItemColors().register((stack, tintIndex) -> tintIndex == 0 ? MultiColoredItem.getColor(stack) : DyeColor.WHITE.getColorValue(), item);
+            event.getItemColors().register((stack, tintIndex) -> tintIndex == 0 ? MultiColoredItem.getColor(stack) : DyeColor.WHITE.getColorValue(), item);
         }
-        minecraft.getItemColors().register((stack, tintIndex) -> {
+        event.getItemColors().register((stack, tintIndex) -> {
             switch(tintIndex) {
                 case 0:
                     return MacaronItem.getTopColor(stack);
@@ -30,6 +29,6 @@ public class ItemColorsCulinaryArts {
             }
             return 0;
         }, ItemsCulinaryArts.MACARON.get());
-        minecraft.getItemColors().register((stack, tintIndex) -> tintIndex == 0 ? PizzaItem.getSauceColor(stack) : DyeColor.WHITE.getColorValue(), ItemsCulinaryArts.PIZZA.get());
+        event.getItemColors().register((stack, tintIndex) -> tintIndex == 0 ? PizzaItem.getSauceColor(stack) : DyeColor.WHITE.getColorValue(), ItemsCulinaryArts.PIZZA.get());
     }
 }
