fab8f03ebe00bc9b687738d2274ee3a9f700791c
fallOut015 <captainDoodles264@gmail.com>
2021-07-03 13:22:04 -0700

    added recipe book

---
+++ src/main/java/io/github/fallOut015/culinary_arts/item/RecipeBookItem.java
@@ -0,0 +1,9 @@
+package io.github.fallOut015.culinary_arts.item;
+
+import net.minecraft.item.Item;
+
+public class RecipeBookItem extends Item {
+    public RecipeBookItem(Properties properties) {
+        super(properties);
+    }
+}
